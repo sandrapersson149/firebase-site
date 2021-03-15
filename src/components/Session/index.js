b9b031b95bdc927184c7ea03fb20987334f50228
Sandra Persson <sandra.persson@student.kyh.se>
2021-03-15 09:47:18 +0100

    second commit

---
+++ src/components/Session/index.js
@@ -0,0 +1,12 @@
+
+import AuthUserContext from './context';
+import withAuthentication from './withAuthentication';
+import withAuthorization from './withAuthorization';
+
+
+export {
+  AuthUserContext,
+  withAuthentication,
+  withAuthorization,
+
+};
